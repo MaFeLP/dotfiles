7f045258b963f5539274e310becd43ce118cf371
MaFeLP <60669873+MaFeLP@users.noreply.github.com>
2023-01-10 17:19:34 -0600

    Tried to disable listchars in TTY
    Signed-off-by: MaFeLP <60669873+MaFeLP@users.noreply.github.com>

--- packages/neovim-config/files/opt.lua
+++ packages/neovim-config/files/opt.lua
@@ -6,13 +6,18 @@ opt.gdefault = true            -- Use 'g' flag by default with :s/foo/bar/.
 opt.encoding="UTF-8"           -- Enable UFT-8 encoding
 opt.tabstop = 4                -- length of an actual \t character:
 opt.termguicolors = true       -- Make colors nice
+opt.foldlevel = 9              -- Fix everything being folded on startup
+opt.expandtab = true           -- Always use spaces instead of tabs
 
 -- Enable other non-printable characters
 -- TODO make this into a toggle
 opt.list = true
-opt.listchars="eol:⏎,tab:⇥ ,trail:␠,space:⸳"
 -- Make tab characters different
 --vim.g.indentLine_char_list = ['|', '¦', '┆', '┊']
+-- Only enable special characters in non TTYs
+if not string.match(os.getenv("TTY") or "", "/dev/tty[0-9]+") then
+  opt.listchars="eol:⏎,tab:⇥ ,trail:␠,space:⸳"
+end
 
 -- Enable powerline fonts for the status bar
 vim.g.airline_powerline_fonts = 1
