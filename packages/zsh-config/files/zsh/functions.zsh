5d623cceaa50830bd4750ae2a9fc7b4981ff9220
MaFeLP <60669873+MaFeLP@users.noreply.github.com>
2023-05-09 23:19:02 -0500

    ⚓ Moved from docker to podman interface in the zsh functions
    Signed-off-by: MaFeLP <60669873+MaFeLP@users.noreply.github.com>

--- packages/zsh-config/files/zsh/functions.zsh
+++ packages/zsh-config/files/zsh/functions.zsh
@@ -5,20 +5,20 @@ source /usr/share/makepkg/util/message.sh
 function md2html() {
   if [ "$1" = "-h" -o "$1" = "--help" -o -z "$1" ]
   then
-    echo "This function takes one argument: The markdown file to convert to html"
+    printf "This function takes one argument: The markdown file to convert to html\n"
   else
     md_name="$1"
     html_name="$(echo "$md_name" | sed -e 's/\.md$/.html/g')"
     if [ "$html_name" = "$md_name" ]
     then
-      echo "File does not end in .md!"
+      printf "File does not end in .md!\n"
     else
-      echo "Converting '$md_name' to '$html_name'..."
+      printf "Converting '$md_name' to '$html_name'...\n"
       if [ -e "$md_name" ]
       then
         pandoc -o "$html_name" "$md_name" --standalone --mathjax
       else
-        echo "Sorry, $md_name does not exist!"
+        prinf "Sorry, $md_name does not exist!\n"
       fi
     fi
   fi
@@ -29,8 +29,8 @@ function nginx-start {
   config=$(mktemp)
   cat > $config <<EOF
 server {
-    listen       80;
-    listen  [::]:80;
+    listen       8080;
+    listen  [::]:8080;
     server_name  localhost;
 
     location / {
@@ -49,49 +49,42 @@ server {
 }
 EOF
 
-  echo -e "\033[0;1m\033[0;96mStarting nginx on port 80 in the current directory.\nPress Ctrl+C to exit.\033[0;0m"
-  docker run --rm -it \
+  printf "\033[0;1m\033[0;96mStarting nginx on port 8080 in the current directory.\nPress Ctrl+C to exit.\033[0;0m\n"
+  podman run --rm -it \
     --name nginx \
     -v "$(pwd):/usr/share/nginx/html:ro" \
     -v "$config:/etc/nginx/conf.d/default.conf:ro"\
-    -p "80:80" \
-    nginx:latest
+    -p "8080:8080" \
+    docker.io/library/nginx:latest
 }
 
-function wayd {
-  case "$1" in
-    start)
-      echo -e "\033[0;1m\033[0;96mStarting waydroid...\nenter 'wayd exit' to exit.\033[0;0m"
-      sudo systemctl start waydroid-container.service
-      waydroid session start &
-      ;;
-    stop)
-      echo -e "\033[0;1m\033[0;96mStarting waydroid...\nenter 'wayd exit' to exit.\033[0;0m"
-      waydroid session stop
-      sudo systemctl stop waydroid-container.service
-      ;;
-    ui)
-      waydroid show-full-ui
-      ;;
-    *)
-      cat <<EOF
-wayd is a Waydroid helper function. Avaliable options:
+function hugo-proxy {
+  local config
+  config=$(mktemp)
+  cat > $config <<EOF
+server {
+    listen       8080;
+    listen  [::]:8080;
+    server_name  localhost;
 
-  wayd start      Starts the Waydroid session
-  wayd stop       Stops the Waydroid session
-  wayd ui         Shows the full UI of waydroid
-  wayd help       Displays this help text
+    location / {
+        proxy_pass http://localhost:1313;
+    }
+
+    # redirect server error pages to the static page /50x.html
+    error_page   500 502 503 504  /50x.html;
+    location = /50x.html {
+        root   /usr/share/nginx/html;
+    }
+}
 EOF
-      ;;
-  esac
-}
 
-function hugo-new {
-  name="$(date +'%Y-%m-%d')-$1"
-  echo "Creating new blog entry: $name"
-  hugo new "blog/$name.md"
-  mkdir -v "content/blog/$name/"
-  mv "content/blog/$name.md" "content/blog/$name/index.md"
-  $EDITOR "content/blog/$name/index.md"
+  printf "\033[0;1m\033[0;96mStarting nginx on port 8080 as a proxy to port 1313.\nPress Ctrl+C to exit.\033[0;0m\n"
+  podman run --rm -it \
+    --name hugo-proxy \
+    -v "$(pwd):/usr/share/nginx/html:ro" \
+    -v "$config:/etc/nginx/conf.d/default.conf:ro"\
+    --network=host \
+    docker.io/library/nginx:latest
 }
 
