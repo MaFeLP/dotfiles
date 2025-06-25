55887c9352fe24f7e29ea5bc2d6a286f2c712d39
MaFeLP <mafelp@proton.me>
2025-06-25 13:38:19 +0200

    [neovim] Fix Trouble not starting with keys shortcut
    Signed-off-by: MaFeLP <mafelp@proton.me>

--- packages/neovim-config/files/plugins.lua
+++ packages/neovim-config/files/plugins.lua
@@ -186,16 +186,16 @@ require("lazy").setup({
   -- Buffer with diagnostics data
   {
     "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    lazy = true,
     keys = {
-        { "<leader>xc", function() require('trouble').toggle() end, desc = "Toggle Diagnostics Window" },
-        { "<leader>xw", function() require('trouble').open('workspace_diagnostics') end, desc = "Open Workspace Diagnostics" },
-        { "<leader>xx", function() require('trouble').open('document_diagnostics') end, desc = "Open Document Diagnostics" },
-        { "<leader>xq", function() require('trouble').open('quickfix') end, desc = "Show Quick Fixes" },
-        { "<leader>xl", function() require('trouble').open('loclist') end, desc = "Window Locations" },
-        { "<leader>xr", function() require('trouble').open('lsp_references') end, desc = "Show references" },
-        { "<leader>xd", function() require('trouble').open('lsp_definitions') end, desc = "Show definitions" },
+      { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)", },
+      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)", },
+      { "<leader>xs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (Trouble)", },
+      { "<leader>xl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP Definitions / references / ... (Trouble)", },
+      { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)", },
+      { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)", },
     },
   },
 
@@ -214,29 +214,11 @@ require("lazy").setup({
           { "<leader>d", group = "Debugging ..." },
           { "<leader>f", group = "Find ..." },
           { "<leader>g", group = "Grammar ..." },
-          { "<leader>t", group = "Diagnostics ..." },
+          { "<leader>x", group = "Diagnostics ..." },
         })
     end,
     dependencies = { "echasnovski/mini.icons" },
   },
-
-  -- Spell Checking
-  -- Spell checking might also be handled by ltex language server!
-  --{
-  --  'rhysd/vim-grammarous',
-  --  lazy = true,
-  --  keys = {
-  --    { "<leader>gg", "<cmd>GrammarousCheck<cr>", desc = "Start Grammar Check" },
-  --    { "<leader>go", "<Plug>(grammarous-open-info-window)", desc = "Open Grammar Window" },
-  --    { "<leader>gc", "<Plug>(grammarous-close-info-window)", desc = "Close Grammar Window" },
-  --    { "<Leader>gf", "<Plug>(grammarous-fixit)", desc = "Fix grammar mistake" },
-  --  },
-  --  init = function()
-  --    -- fix error when loading the latest LanguageTool version
-  --    -- TODO: Fork and update to latest version
-  --    vim.g.grammarous_jar_url = 'https://www.languagetool.org/download/LanguageTool-5.9.zip'
-  --  end,
-  --},
 
   -- Colorschemes
   { "dracula/vim", lazy = true, name = "dracula" },  -- Dracula color scheme
